db394b11dfeb84c957a9ea20d7e02d84f6333585
pong3ds <chaiyapong@3dsinteractive.com>
2021-11-30 13:42:05 +0700

    Increase font size

--- src/stoken.d.ts
+++ src/stoken.d.ts
@@ -1,22 +1,23 @@
-import { Network } from './consts'
-import { ERC20TokenModel } from './models'
-import '../css/styles.scss'
+import { Network } from './consts';
+import { ERC20TokenModel } from './models';
+import '../css/styles.scss';
 export default class SToken {
-  private network
-  private merchantAddr
-  constructor(network: Network, merchantAddr: string)
-  showPaymentDialog(orderNumber: number, amountUSD: number): void
-  private showQR
-  private hideQR
-  private getDialogHTML
-  private buildMobilePaymentURL
-  private getRandomId
-  get MPOINT(): ERC20TokenModel
-  get CPOINT(): ERC20TokenModel
-  get SUSD(): ERC20TokenModel
-  get DAI(): ERC20TokenModel
-  get USDC(): ERC20TokenModel
-  get USDT(): ERC20TokenModel
-  get BUSD(): ERC20TokenModel
-  get chainId(): string
+    private network;
+    private merchantAddr;
+    constructor(network: Network, merchantAddr: string);
+    showPaymentDialog(orderNumber: string, amountUSD: number): void;
+    private showQR;
+    private hideQR;
+    private getDialogHTML;
+    private buildDesktopPaymentURL;
+    private buildMobilePaymentURL;
+    private getRandomId;
+    get MPOINT(): ERC20TokenModel;
+    get CPOINT(): ERC20TokenModel;
+    get SUSD(): ERC20TokenModel;
+    get DAI(): ERC20TokenModel;
+    get USDC(): ERC20TokenModel;
+    get USDT(): ERC20TokenModel;
+    get BUSD(): ERC20TokenModel;
+    get chainId(): string;
 }
